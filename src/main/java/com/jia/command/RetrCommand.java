badc607573f5bdb5da45593028be17ba450eb8bf
Jia <linjinjia047@163.com>
2018-06-25 01:39:58 +0800

    FTPServer

--- src/main/java/com/jia/command/RetrCommand.java
+++ src/main/java/com/jia/command/RetrCommand.java
@@ -2,16 +2,49 @@ package com.jia.command;
 
 import com.jia.thread.ControllerThread;
 
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 /**
  * @author jia
  * @date 2018/6/22 22:32
+ * 从服务器下载文件
  **/
 public class RetrCommand implements Command {
 
     @Override
     public void execute(String data, PrintWriter out, ControllerThread thread) {
-
+        // 要发送的文件
+       String filePath = thread.getNowDir() + File.separator + data;
+       File file = new File(filePath);
+       if(!file.exists()){
+           out.println("220 File not exists");
+           out.flush();
+       }else{
+           try{
+               out.println("150 Open ascii mode");
+               out.flush();
+               Socket socket = new Socket(thread.getTargetIP(), thread.getTargetPort());
+               // 读取服务器文件的输出流
+               FileInputStream fis = new FileInputStream(file);
+               BufferedReader br = new BufferedReader(new InputStreamReader(fis));
+               // 发送数据的 socket 的输出流
+               PrintWriter dataOut = new PrintWriter(socket.getOutputStream());
+               // 判断输入流是否读完的标志
+               String s = null;
+               while((s = br.readLine()) != null){
+                    dataOut.println(s);
+               }
+               dataOut.flush();
+               fis.close();
+               dataOut.close();
+               socket.close();
+               out.println("220 Transfer complete ");
+           } catch (Exception e){
+               out.println("220 Transmission interruption");
+           }
+           out.flush();
+       }
     }
 }
--- src/main/java/com/jia/command/StoreCommand.java
+++ src/main/java/com/jia/command/StoreCommand.java
@@ -2,16 +2,42 @@ package com.jia.command;
 
 import com.jia.thread.ControllerThread;
 
-import java.io.PrintWriter;
-import java.io.Writer;
+import java.io.*;
+import java.net.Socket;
 
 /**
  * @author jia
  * @date 2018/6/22 22:33
+ * 从客户端上传文件
  **/
 public class StoreCommand implements Command {
     @Override
     public void execute(String data, PrintWriter out, ControllerThread thread) {
-
+        out.println("150 Binary data connection\r\n");
+        out.flush();
+        File file = new File(thread.getNowDir() + File.separator + data);
+        try {
+            Socket socket = new Socket(thread.getTargetIP(), thread.getTargetPort());
+            // socket 的数据输入流
+            InputStreamReader dataIn = new InputStreamReader(socket.getInputStream(), "UTF-8");
+            BufferedReader br = new BufferedReader(dataIn);
+            // 要写入的文件
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+            // 判断输入流是否读完
+            String s = null;
+            while((s = br.readLine()) != null){
+                bw.write(s);
+                bw.newLine();
+            }
+            bw.flush();
+            out.println("226 Transfer complete");
+            out.flush();
+            dataIn.close();
+            br.close();
+            bw.close();
+            socket.close();
+        } catch (IOException e) {
+            out.println("226 Transmission interruption");
+        }
     }
 }
