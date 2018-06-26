2f80245b07b641826957da38b029bcf32c5ac8fd
Jia <linjinjia047@163.com>
2018-06-26 20:00:09 +0800

    FTPServer

--- src/main/java/com/jia/utils/StringUtils.java
+++ src/main/java/com/jia/utils/StringUtils.java
@@ -1,13 +1,17 @@
 package com.jia.utils;
 
-import java.util.Formatter;
-
 /**
  * @author jia
  * @date 2018/6/26 19:36
  **/
 public class StringUtils {
 
+    /**
+     * 将字符串左对齐并占用length的长度
+     * @param length 长度
+     * @param s 字符串
+     * @return 格式化后的字符串
+     */
     public static String format(Integer length, String s){
         StringBuilder format = new StringBuilder();
         format.append("%").append(-length).append("s");
